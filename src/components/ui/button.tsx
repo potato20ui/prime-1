3295015e0d6143d550ac51e5a135af6072431833
Rhon Edrian Mercado <rhoed.mercado@gmail.com>
2025-04-28 14:07:24 +0800

    Delete src directory

--- src/components/ui/button.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-  return <button {...props}>{children}</button>;
-};
-
-export default Button;
