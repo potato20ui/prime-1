9506056867ea09c5283e17f1f17b211bac51c1ae
ChantiPHP <chrstneclps@gmail.com>
2025-04-29 15:29:29 +0800

    Update Input.tsx

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,26 +1,7 @@
-// src/components/ui/Input.tsx
+import React from "react";
 
-import * as React from "react"
-import { cn } from "@/lib/utils" // Same utility function for merging classes
+const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
+  return <input {...props} />;
+};
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> { }
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ className, type, ...props }, ref) => {
-        return (
-            <input
-                type={type}
-                className={cn(
-                    "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-                    className
-                )}
-                ref={ref}
-                {...props}
-            />
-        )
-    }
-)
-
-Input.displayName = "Input"
-
-export { Input }
+export default Input;
